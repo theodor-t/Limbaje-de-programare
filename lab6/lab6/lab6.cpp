6784a6064c76ebc18b60dc19eec5e27886cfd620
Theodor <54949643+theodorerlax@users.noreply.github.com>
2020-05-26 12:06:45 +0300

    Add files via upload

---
+++ lab6/lab6/lab6.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int a;
+    string path = "myFile.txt";
+   /* ofstream fout;
+    
+    fout.open(path,ofstream::app);
+    if (!fout.is_open())
+    {
+        cout << "Eroare de deschidere a fisierului!" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "Introduceti text: " ;
+            cin >> a;
+            fout << a;
+            fout << "\n";
+        }
+        
+    }
+    fout.close();
+    */
+
+    ifstream fin;
+    fin.open(path);
+    if (!fin.is_open())
+    {
+        cout << "Eroare de deschidere a fisierului!" << endl;
+    }
+    else
+    {
+        cout << "Fisierul a fost deschis cu succes!" << endl;
+        char ch;
+        while(fin.get(ch))
+        {
+            cout << ch;
+        }
+    }
+    fin.close();
+    return 0;
+}
+
